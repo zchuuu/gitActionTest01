3627a2d7f77bef98c280656edb3075caad69f262
PJS <jisoopark8@gmail.com>
2024-10-14 21:47:22 +0900

    version: 1->2->3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // computes a factorial of a given positive integer (양수에 대해서 팩토리얼 계산)
-// version: 1 -> 2
+// version: 1 -> 2 -> 3
 function factorial(n) {     // 5! = 5 * 4 * 3 * 2 * 1 = 120
 
     // n이 음수인 경우
